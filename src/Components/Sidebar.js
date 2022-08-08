c88132e1559865ccef8ebecd2af1d897b69bfa06
krzysztof.grabowski <krzysztof.grabowski@cronn.de>
2022-08-09 00:14:10 +0200

    Initial commit

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../App.css';
+import {SidebarDat} from "./SidebarData";
+
+function Sidebar() {
+    return <div className="Sidebar">
+        <ul className="SidebarList">
+            {SidebarDat.map((val, key) => {
+                return (
+                    <li key={key} className="row" onClick={() => {
+                        window.location.pathname = val.link
+                    }}>
+                        <div id="icon">{val.icon}</div>
+                        <div id="title">{val.title}</div>
+                    </li>
+                );
+            })};
+        </ul>
+    </div>
+
+}
+
+export default Sidebar
---
+++ src/Components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from "react";
+import LanIcon from '@mui/icons-material/Lan';
+
+export const SidebarDat = [
+    {
+        title: "Home",
+        icon: <LanIcon/>,
+        link: "/home"
+    },
+    {
+        title: "Home",
+        icon: <LanIcon/>,
+        link: "/home"
+    },
+    {
+        title: "Home",
+        icon: <LanIcon/>,
+        link: "/home"
+    }
+]
